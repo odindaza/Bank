cbff45d4125ac5c27aaade3aa45c595a107c1686
Odin Daza <106930530+odindaza@users.noreply.github.com>
2023-02-01 08:47:37 -0500

    Adding the classes

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Classes;
+
+var account = new BankAccount("Nick", 1000);
+Console.WriteLine($"Account {account.Number} was created for {account.Owner}" +
+    $"with {account.Balance} initial balance");
+
